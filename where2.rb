28bb0415996a77ec7af4a1ffeb9096cd4a373a26
clements-dev <clement.safir@kellogg.northwestern.edu>
2020-02-18 02:44:15 +0000

    new2

--- where2.rb
+++ where2.rb
@@ -4,6 +4,12 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
+    # 42.0574063
+    # -87.6722787
   # lat: ± 90
   # long: ± 180
+  @lat = rand(-90.0..90.0)
+  @long = rand(-180.0..180.0)
+  @lat_long = "#{@lat}, #{@long}"
+  view "where2"
 end
